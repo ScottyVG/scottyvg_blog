c96224d34ba5082905c30a79f0c66409aa020a2c
Scott Van Gilder <svangilder@gmail.com>
2016-10-19 15:51:01 -0600

    exports naming convention

--- mw/qblog.js
+++ mw/qblog.js
@@ -16,11 +16,13 @@ getBlogByID(id) => {
 
 getBlogWithCommentByBlogID(id) {
   return knex('blog')
-    .select('blog.id AS blog_id')
+    .select('blog.id AS blog_id', 'blog.title', 'blog.content AS blogContent', 'blog.imageURL', 'comment.id AS comment_id', 'comment.user_id AS comment_user', 'comment.content AS comment_content')
+    .fullOuterJoin('comment', 'blog.id', 'comment.blog_id')
+    .where('blog.id', id)
 }
 
 module.exports = {
   getAllBlogs: getAllBlogs,
   getBlogByID: getBlogByID,
-  getBlogAndCommentsByBlogID: getBlogAndCommentsByBlogID
+  getBlogWithCommentByBlogID: getBlogWithCommentByBlogID
 }
