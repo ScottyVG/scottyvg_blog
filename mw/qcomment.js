e78d5d990c959e42b43df12ff739bab4d5c94dd9
Scott Van Gilder <svangilder@gmail.com>
2016-10-20 10:44:39 -0600

    comment middleware complete

--- mw/qcomment.js
+++ mw/qcomment.js
@@ -2,3 +2,56 @@
 
 const bcrypt = require('bcrypt')
 const knex = require('./db/knex.js')
+
+//Query Comment db
+getComments(blog_id) => {
+  return knex('comment')
+    .select('comment.id AS comment_id', 'comment.blog_id', 'user.fullName AS comment_user', 'comment.content as comment_content', 'user.id AS user_id')
+    .innerJoin('user', 'comment.user_id', 'user.id')
+    .where('comment.blog_id', blog_id)
+    .orderBy('comment.id', 'desc');
+}
+
+getCommentsByID(comment) => {
+  return knex('comment')
+    .select('id AS comment_id', 'blog_id', 'content AS comment_content', 'user_id')
+    .where('id', comment_id)
+}
+
+createComment(user_id, blog_id, content, user_fullName) => {
+  return knex('comment')
+    .insert({
+      user_id: user_id,
+      blog_id: blog_id,
+      content: content,
+      user_fullName: user_fullName
+    })
+}
+
+editComment(comment_id, content) => {
+  return knex('comment').update({
+      content: content
+    })
+    .where('id', comment_id)
+}
+
+deleteComment(comment_id) => {
+  return knex('comment')
+    .where('id', comment_id)
+    .del()
+}
+
+deleteComments(blog_id) => {
+  return knex('comment')
+    .where('blog_id', blog_id)
+    .del()
+}
+
+module.exports = {
+  getComments: getComments,
+  getCommentsByID: getCommentsByID,
+  createComment: createComment,
+  editComment: editComment,
+  deleteComment: deleteComment,
+  deleteComments: deleteComments
+}
