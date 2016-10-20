9b9566c6af7c96f1e30472d14d1a8185969b20b1
Scott Van Gilder <svangilder@gmail.com>
2016-10-20 10:35:05 -0600

    complete qblog middleware

--- mw/qblog.js
+++ mw/qblog.js
@@ -23,14 +23,55 @@ getBlogWithCommentByBlogID(id) {
     .where('blog.id', id)
 }
 
+// TODO: add to middleware after this line
 getBlogByTitle(title) => {
   return knex('blog')
     .select('id')
     .where('title', title)
 }
 
+getBlogByTitle(title) => {
+  return knex('blog')
+    .select('id')
+    .where('title', title)
+}
+
+createBlogPost(title, content, image, user_id, user_fullname, snippet) => {
+  return knex('blog')
+    .insert({
+      title: title,
+      content: content,
+      imageURL: image,
+      user_id: user_id,
+      user_fullName: user_fullName,
+      snippet: content.substring(0, 200) + '...'
+    })
+}
+
+editBlogPost(blog_id, title, content, image, snippet) => {
+  return knex('blog')
+    .update({
+      title: title,
+      content: content,
+      imageURL: image,
+      snippet: content.substring(0, 200) + '...'
+    })
+    .where('id', blog_id)
+}
+
+deleteBlogPost(blog_id) => {
+  return knex('blog')
+    .where('id', blog_id)
+    .del()
+}
+
 module.exports = {
   getAllBlogs: getAllBlogs,
   getBlogByID: getBlogByID,
-  getBlogWithCommentByBlogID: getBlogWithCommentByBlogID
+  getBlogWithCommentByBlogID: getBlogWithCommentByBlogID,
+  getBlogByTitle: getBlogByTitle,
+  getBlogByTitle: getBlogByTitle,
+  createBlogPost: createBlogPost,
+  editBlogPost: editBlogPost,
+  deleteBlogPost: deleteBlogPost
 }
