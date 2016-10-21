4000f16f8b248437f8c4b5e221a3d61e03861a89
Scott Van Gilder <svangilder@gmail.com>
2016-10-21 00:00:16 -0600

    migrate to api middleware folder

--- mw/api.js
+++ mw/api.js
@@ -5,169 +5,127 @@ const knex = require('../db/knex.js')
 
 //Knex to Users db Table
 function hashPassword(password) {
-  return bcrypt.hashSync(password, 10)
+  return bcrypt.hashSync(password, 10);
 }
 
 function findUser(username) {
-  return knex('users')
-    .select('username')
-    .where('username', username)
-    .first()
+  return knex('users').select('username').where('username', username).first();
 }
 
 function findUserInfo(username) {
-  return knex('users')
-    .select('id', 'username', 'fullName')
-    .where('username', username)
-    .first()
+  return knex('users').select('id', 'username', 'fullName').where('username', username).first();
 }
 
 function findHashedPassword(username) {
-  return knex('users')
-    .select('user.password')
-    .where('user.username', username)
-    .first()
+  return knex('users').select('user.password').where('user.username', username).first();
 }
 
 function authenticateUser(username, password) {
   return findUser(username)
-    .then((userData) => {
+    .then(function(userData) {
       if (!userData) {
-        return false
+        return false;
       }
       return findHashedPassword(username)
-        .then((hashedPassword) => {
-          hashedPassword = hashedPassword.password
-          return bcrypt.compareSync(password, hashedPassword)
-        })
-    })
+        .then(function(hashedPassword) {
+          hashedPassword = hashedPassword.password;
+          return bcrypt.compareSync(password, hashedPassword);
+        });
+    });
 }
 
 function addUser(username, password, fullName) {
   if (!username || !password || !fullName) {
-    return false
+    return false;
   }
   return findUser(username)
-    .then((data) => {
+    .then(function(data) {
       if (data) {
-        return false
+        return false;
       }
-      return knex('users')
-        .insert({
-          username: username,
-          password: hashPassword(password),
-          admin: true,
-          fullName: fullName
-        })
+      return knex('users').insert({
+        username: username,
+        password: hashPassword(password),
+        admin: true,
+        fullName: fullName
+      });
     })
-    .catch((err) => {
-      return err
-    })
+    .catch(function(err) {
+      return err;
+    });
 }
+
 
 //Knex to Blog db Table
 function getAllBlogs() {
-  return knex('blogs')
-    .select('id AS blogs_id', 'title', 'snippet', 'imageURL', 'users_fullName')
-    .orderBy('id', 'desc')
+  return knex('blogs').select('id AS blog_id', 'title', 'snippet', 'imageURL', 'users_fullName').orderBy('id', 'desc');
 }
 
 function getBlogByID(id) {
-  return knex('blogs')
-    .select('id', 'title', 'content AS blogsContent', 'users_id', 'imageURL', 'users_fullName')
-    .where('id', id)
+  return knex('blogs').select('id', 'title', 'content AS blogContent', 'users_id', 'imageURL', 'users_fullName').where('id', id);
 }
 
-function getBlogWithCommentByBlogID(id) {
-  return knex('blogs')
-    .select('blogs.id AS blogs_id', 'blogs.title', 'blogs.content AS blogsContent', 'blogs.imageURL', 'comments.id AS comments_id', 'comments.users_id AS comments_user', 'comments.content AS comments_content')
-    .fullOuterJoin('comments', 'blogs.id', 'comments.blogs_id')
-    .where('blogs.id', id)
+function getBlogAndCommentsByBlogID(id) {
+  return knex('blogs').select('blog.id AS blog_id', 'blog.title', 'blog.content AS blogContent', 'blog.imageURL', 'comment.id AS comment_id', 'comment.users_id AS comment_users', 'comment.content AS comment_content').fullOuterJoin('comment', 'blog.id', 'comment.blog_id').where('blog.id', id);
 }
 
 function getBlogByTitle(title) {
-  return knex('blogs')
-    .select('id')
-    .where('title', title)
+  return knex('blogs').select('id').where('title', title);
 }
 
-function getBlogByTitle(title) {
-  return knex('blogs')
-    .select('id')
-    .where('title', title)
+function getComments(blog_id) {
+  return knex('comment').select('comment.id AS comment_id', 'comment.blog_id', 'user.fullName AS comment_users', 'comment.content as comment_content', 'user.id AS users_id').innerJoin('users', 'comment.users_id', 'users.id').where('comment.blog_id', blog_id).orderBy('comment.id', 'desc');
 }
 
-function createBlogPost(title, content, image, users_id, users_fullname, snippet) {
-  return knex('blogs')
-    .insert({
-      title: title,
-      content: content,
-      imageURL: image,
-      users_id: users_id,
-      users_fullName: users_fullName,
-      snippet: content.substring(0, 200) + '...'
-    })
+function getCommentsByID(comment_id) {
+  return knex('comment').select('id AS comment_id', 'blog_id', 'content AS comment_content', 'users_id').where('id', comment_id);
 }
 
-function editBlogPost(blogs_id, title, content, image, snippet) {
-  return knex('blogs')
-    .update({
-      title: title,
-      content: content,
-      imageURL: image,
-      snippet: content.substring(0, 200) + '...'
-    })
-    .where('id', blogs_id)
+function createPost(title, content, image, users_id, users_fullName, snippet) {
+  return knex('blog').insert({
+    title: title,
+    content: content,
+    imageURL: image,
+    users_id: users_id,
+    users_fullName: users_fullName,
+    snippet: content.substring(0, 200) + '...'
+  });
 }
 
-function deleteBlogPost(blogs_id) {
-  return knex('blogs')
-    .where('id', blogs_id)
-    .del()
+function createComment(users_id, blog_id, content, users_fullName) {
+  return knex('comment').insert({
+    users_id: users_id,
+    blog_id: blog_id,
+    content: content,
+    users_fullName: users_fullName
+  });
 }
 
-//Knex to Comments db Table
-function getComments(blogs_id) {
-  return knex('comments')
-    .select('comments.id AS comments_id', 'comments.blogs_id', 'user.fullName AS comments_user', 'comments.content as comments_content', 'user.id AS users_id')
-    .innerJoin('users', 'comments.users_id', 'user.id')
-    .where('comments.blogs_id', blogs_id)
-    .orderBy('comments.id', 'desc');
+function editBlogPost(blog_id, title, content, image, snippet) {
+  return knex('blog').update({
+    title: title,
+    content: content,
+    imageURL: image,
+    snippet: content.substring(0, 200) + '...',
+  }).where('id', blog_id);
 }
 
-function getCommentsByID(comments) {
-  return knex('comments')
-    .select('id AS comments_id', 'blogs_id', 'content AS comments_content', 'user_id')
-    .where('id', comment_id)
+function deleteComments(blog_id) {
+  return knex('comment').where('blog_id', blog_id).del();
 }
 
-function createComment(users_id, blogs_id, content, users_fullName) {
-  return knex('comments')
-    .insert({
-      users_id: users_id,
-      blogs_id: blogs_id,
-      content: content,
-      users_fullName: users_fullName
-    })
+function deleteBlogPost(blog_id) {
+  return knex('blog').where('id', blog_id).del();
 }
 
-function editComment(comments_id, content) {
-  return knex('comments').update({
-      content: content
-    })
-    .where('id', comments_id)
+function editComment(comment_id, content) {
+  return knex('comment').update({
+    content: content
+  }).where('id', comment_id);
 }
 
-function deleteComment(comments_id) {
-  return knex('comments')
-    .where('id', comments_id)
-    .del()
-}
-
-function deleteComments(blogs_id) {
-  return knex('comments')
-    .where('blogs_id', blogs_id)
-    .del()
+function deleteComment(comment_id) {
+  return knex('comment').where('id', comment_id).del();
 }
 
 module.exports = {
@@ -177,10 +135,10 @@ module.exports = {
   addUser: addUser,
   getAllBlogs: getAllBlogs,
   getBlogByID: getBlogByID,
-  getBlogWithCommentByBlogID: getBlogWithCommentByBlogID,
+  getBlogAndCommentsByBlogID: getBlogAndCommentsByBlogID,
   getBlogByTitle: getBlogByTitle,
   getBlogByTitle: getBlogByTitle,
-  createBlogPost: createBlogPost,
+  createPost: createPost,
   editBlogPost: editBlogPost,
   deleteBlogPost: deleteBlogPost,
   getComments: getComments,
